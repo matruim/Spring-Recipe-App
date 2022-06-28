f72b69c082bde308b50d0facc023c4b5e1998599
Jared Good <jared_danell@yahoo.com>
2022-06-28 14:08:29 -0600

    Project Lombok Refactor

--- src/main/java/com/jaredgood/springrecipeapp/services/RecipeServiceImpl.java
+++ src/main/java/com/jaredgood/springrecipeapp/services/RecipeServiceImpl.java
@@ -2,11 +2,13 @@ package com.jaredgood.springrecipeapp.services;
 
 import com.jaredgood.springrecipeapp.domain.Recipe;
 import com.jaredgood.springrecipeapp.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -18,6 +20,8 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("I'm in the RecipeServiceImpl");
+
         Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
